c7ecd88291409957197251b40cf2cf9c53c2a40b
Marek <marek.wyszynski@bitapps.fi>
2024-03-04 21:32:37 +0100

    wip: landing page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {/* top left */}
+      <div className='app_text_name'>
+        <div>
+          <span style={{ 'color': '#32a864', 'fontWeight': '500' }}>GIS </span>DEV
+        </div>
+        <div>
+          <a href=""><span style={{ 'fontSize': '0.8em' }}>Marek Wyszyński</span></a>
+        </div>
+      </div>
+
+      {/* top right */}
+      <div className='app_text_full_stack'>
+        <a href="https://github.com/gitgeoman">FULL STACK WEB & DATA DEVELOPER &#8599;</a>
+      </div>
+
+      {/* bottom right */}
+      <div className='app_text_email'>
+        <a href="mailto:gitgeoman@gmail.com">EMAIL</a>
+      </div>
     </div>
   );
 }
