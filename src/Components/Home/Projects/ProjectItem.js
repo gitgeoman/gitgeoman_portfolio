2a73b888e99f9afa3b4574455daf98b460cb8e28
Marek <marek.wyszynski@bitapps.fi>
2024-03-17 22:47:00 +0100

    wip add todo

--- src/Components/Home/Projects/ProjectItem.js
+++ src/Components/Home/Projects/ProjectItem.js
@@ -27,14 +27,13 @@ function ProjectItem({ name, description, stack, link, img }) {
       <div className="ProjectItem" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}
         style={{ display: 'flex', alignItems: 'flex-end', backgroundImage: `url(${img})`, backgroundPosition: 'center', backgroundSize: 'auto 100%', backgroundRepeat: "no-repeat" }}
       >
+
         <Card style={{ background: 'none', color: '#32a864', width: '100%', height: '15%', visibility: isVisible }} >
           <CardContent >
             <Typography gutterBottom variant="h5" component="div">
               {name}
             </Typography>
-
           </CardContent>
-
         </Card>
       </div >
 
