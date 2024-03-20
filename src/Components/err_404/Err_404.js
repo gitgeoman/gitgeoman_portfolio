936d6d4512541bdc126a7f9caf34a2f4c2885931
Marek Wyszyński <45630165+gitgeoman@users.noreply.github.com>
2024-03-20 12:14:14 +0100

    wip: update err page

--- src/Components/err_404/Err_404.js
+++ src/Components/err_404/Err_404.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import PublicIcon from '@mui/icons-material/Public';
+import { NavLink } from 'react-router-dom';
+
+import './Err_404.css'
+
 
 function Err_404() {
   return (
     <div className='err_404'>
-      <div>This is custom err website</div>
+
+      <NavLink to="/">
+
+        <div className='err_404'><PublicIcon style={{ fontSize: 40 }} /></div>
+      </NavLink>
+      <div>
+        <span style={{ color: "#32a864", fontWeight: "500" }}>GIS </span>
+        DEV
+      </div>
+
     </div>
   )
 }
